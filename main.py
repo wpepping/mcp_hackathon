48108456ce4fd70d3bda1fcaa2fb51a9adc38d06
Wouter Pepping <w.pepping@zandersgroup.com>
2025-04-03 14:45:19 +0200

    Updated port

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import uvicorn
 from app import configure_app
+from settings import PORT
 
 app = configure_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app, port=8000, log_config=None, proxy_headers=True)
+    uvicorn.run(app, port=PORT, log_config=None, proxy_headers=True)
